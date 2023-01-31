ae764278f9bd918cb71a60ec61cd8c162b17ac40
Anna <kochurovaav@gmail.com>
2023-01-31 13:19:09 +0200

    Forth folder is added

---
+++ Task0402/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+Console.WriteLine("Enter number :");
+
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+int numberK = 2;
+
+if (numberN == 1 || numberK <= 0)
+{
+    Console.WriteLine("Wrong number");
+}
+else
+{
+    Console.Write(numberN+" ->");
+    while (numberK<=numberN)
+    {
+        Console.Write(numberK+" ");
+        numberK += 2;
+    }
+}
